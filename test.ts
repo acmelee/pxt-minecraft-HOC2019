7119b31308c2955d27868f8250549db61cbbc3d8
dedederek <lihang@starwit.com>
2020-01-02 17:39:18 +0800

    Initial commit

---
+++ test.ts
@@ -0,0 +1,16 @@
+hourOfCode.agentAnalyze(SixDirection.Forward);
+hourOfCode.agentDetectDryFern(SixDirection.Forward);
+hourOfCode.agentDetectDryGrass(SixDirection.Forward);
+hourOfCode.agentDetectDryBrush(SixDirection.Forward);
+hourOfCode.hazardsRemainL4();
+hourOfCode.hazardsRemainL5();
+hourOfCode.hazardsRemainL6();
+hourOfCode.agentDestroyL4(SixDirection.Forward);
+hourOfCode.agentDestroyL5(SixDirection.Forward);
+hourOfCode.agentDestroyL6(SixDirection.Forward);
+hourOfCode.openDoor();
+hourOfCode.completeTask();
+hourOfCode.agentLookForHazards();
+hourOfCode.agentSeeHazard();
+hourOfCode.alertTeam();
+hourOfCode.fakePlace(Block.Grass, SixDirection.Forward);
